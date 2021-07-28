553fa3266132f9e8b9d4915762a1a6fff05a1422
ikedakohei <kouhik06@gmail.com>
2021-07-29 07:21:13 +0900

    2-4 v-bind, v-for, v-if, v-showの使い方

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
+    pSize: '20px',
+    isCapital: true,
+    products: [
+      {code: 'A01', name: 'プロダクトA'},
+      {code: 'B01', name: 'プロダクトB'},
+      {code: 'C01', name: 'プロダクトC'},
+    ],
+    price: 980,
+    stock: 10,
   },
 });
