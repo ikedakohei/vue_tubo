022edbe3f638d7e44ecef816629c840374d47be9
ikedakohei <kouhik06@gmail.com>
2021-07-30 06:59:13 +0900

    2-5 filters

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const app = new Vue({
   el: '#app',
   data: {
-    pSize: '20px',
-    isCapital: true,
-    products: [
-      {code: 'A01', name: 'プロダクトA'},
-      {code: 'B01', name: 'プロダクトB'},
-      {code: 'C01', name: 'プロダクトC'},
-    ],
-    price: 980,
-    stock: 10,
+    price: 1000
   },
+  filters: {
+    number_format(val) {
+      return val.toLocaleString();
+    },
+    unit(val) {
+      return val + '円';
+    },
+  }
 });
